275578d0ed82ebb796ec66f5a1f9cd88b0e10f62
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-02 16:15:09 +0530

    import authcontext provider in index.js file

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom'
-
+import { AuthContextProvider } from './Auth/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
