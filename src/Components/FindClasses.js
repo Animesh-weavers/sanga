5272c8e49257ea85e584dc363a8cf01b396bc169
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-02 15:13:06 +0530

    install react-router-dom and file structure

---
+++ src/Components/FindClasses.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FindClasses = () => {
+  return (
+    <div>FindClasses</div>
+  )
+}
+
+export default FindClasses
---
+++ src/Components/JoinASATeacher_Page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JoinASATeacher_Page = () => {
+  return (
+    <div>JoinASATeacher_Page</div>
+  )
+}
+
+export default JoinASATeacher_Page
