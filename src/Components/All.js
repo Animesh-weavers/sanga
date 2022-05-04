2994c9d9e73481a046c1df441c602f87a142e77b
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-04 17:28:03 +0530

    remove useEffect import

--- src/Components/All.js
+++ src/Components/All.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
